fee93758347675dfdb8aa2fa8f641289f17604ee
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-03-27 16:58:55 +0100

    error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,67 +1,54 @@
-const Joi = require('joi')
-const User = require('../models/usersModel')
-const catchAsync = require('../utils/catchAsync')
+const Joi = require("joi");
+const User = require("../models/usersModel");
+const catchAsync = require("../utils/catchAsync");
 //const { v4: uuidv4 } = require('uuid')
 
-const getAllUsers = async (req,res)=>{
-    try{
-        const users = await User.find()
-        res.status(200).json({
-            status: 'sucess',
-            data:{
-                users
-            }
-        }) 
-    }catch(err){
-        console.log(err)
-        res.status(404).json({
-            status:'fail',
-            message:err
-        })
-    }
-    
-}
-const getUser = (req,res)=>{
-    res.status(500).json({
-        status: 'error',
-        message:'on the way'
-    })
-}
+const getAllUsers = catchAsync(async (req, res) => {
+    const users = await User.find();
+    res.status(200).json({
+      status: "sucess",
+      data: {
+        users,
+      },
+    });
+});
 
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "on the way",
+  });
+};
 
+const createUser = catchAsync(async (req, res) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordconfirm,
+    role: req.body.role,
+  });
 
-const createUser = catchAsync(async(req,res)=>{
-        const newUser = await User.create({
-            name:req.body.name,
-            email:req.body.email,
-            password:req.body.password,
-            passwordConfirm:req.body.passwordconfirm,
-            role:req.body.role
-    
-        })
-        
-        res.status(200).json({
-            status: 'sucess',
-            data:{
-              user:newUser  
-            }
+  res.status(200).json({
+    status: "sucess",
+    data: {
+      user: newUser,
+    },
+  });
+});
 
-        })
-    } )
-    
-
-const updateUser= (req,res)=>{
-    res.status(500).json({
-        status: 'error',
-        message:'on the way'
-    })
-}
-const deleteUser = (req,res)=>{
-    res.status(500).json({
-        status: 'error',
-        message:'on the way'
-    })
-}
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "on the way",
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "on the way",
+  });
+};
 
 // const userValidation = Joi.object({
 //     email:Joi.string().email().required(),
@@ -70,4 +57,4 @@ const deleteUser = (req,res)=>{
 
 //})
 //userValidation.validate(r)
-module.exports = {getAllUsers,getUser,createUser,updateUser,deleteUser}
+module.exports = { getAllUsers, getUser, createUser, updateUser, deleteUser };
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+process.on('uncaughtException', err=>{
+  console.log('UNCAUGHT EXCEPTION! Shutting down....')
+  console.log(err)
+    process.exit(1)
+  })
+
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
@@ -9,6 +16,15 @@ mongoose
   .connect(mongoUrl)
   .then((con) => console.log("DB connected sucessfully"));
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`joyfully listening on port ${port}`);
 });
+
+process.on('unhandledRejection', err=>{
+  console.log('UNHANDLED REJECTION! Shutting down....')
+  console.log(err)
+  server.close(()=>{
+    process.exit(1)
+  })
+})
+
