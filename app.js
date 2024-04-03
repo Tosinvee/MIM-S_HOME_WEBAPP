c6eff0dc61c27939652cb6c226fdd9799ed52918
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-04-03 13:32:32 +0100

    data sanitiazation

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
-
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +32,14 @@ app.use('/api', limiter)
 
 //body parser middleware that parses the JSON data and makes it available in req.body 
 app.use(express.json())
+
+//Data sanitization against Nosql query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss())
+
+//serving static files
 app.use(express.static(`${__dirname}/public`))
 
 
