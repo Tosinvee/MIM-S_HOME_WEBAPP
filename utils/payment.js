b3dd37649e12ab2384a8d46f07302bf5befd0dc7
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-04-11 20:45:45 +0100

    comment

--- utils/payment.js
+++ utils/payment.js
@@ -1,40 +1,44 @@
-const { response } = require("express")
-
-const payStack = (request) =>{
-
-    const initializePayment = (form, myCallBack) =>{
-        const options = {
-            url :"https://api.paystack.co/transaction/initialize",
-            headers:{
-                authorization: process.env.PAYSTACK_SECRET_KEY,
-                'content-type': 'application/json',
-                'cache-control': 'no-cache'
-            },
-            form
-        }
-        const callBack = (error, response,body) =>{
-            return myCallBack(error, body)
-        }
-        request.post(options, callBack)
-    }
-
-
-const verifyPayment = (ref, myCallBack) =>{
-const options = {
-    url: "https://api.paystack.co/transaction/verify/"+encodeURIComponent(ref),
-    headers:{
+//THIS payStack function is designed to handle payment processing using the Paystack API. It consists of two main functions: initializePayment and verifyPayment.
+const payStack = (request) => {
+  // THIS FUNCTION TAKES TWO PARAMETER FORM AND CALLBACK
+  const initializePayment = (form, myCallBack) => {
+    // THE OPTIONS OBJECT CONTAINS THE URL,HEADER, AND FORM DATA
+    const options = {
+      url: "https://api.paystack.co/transaction/initialize",
+      headers: {
         authorization: process.env.PAYSTACK_SECRET_KEY,
-        'content-type': 'application/json',
-        'cache-control': 'no-cache'
-    }
-}
-const callBack = (error, response,body) =>{
-    return myCallBack(error, body)
-}
-request(options, callBack)
+        "content-type": "application/json",
+        "cache-control": "no-cache",
+      },
+      form, // A FORM DATA REQUIRED FOR MAKING AN HTTP POST REQUEST TO INITIALIZE A PAYMENT THROUGH THE PAYSTACK API
+    };
+    //CALLBACK TAKES 3 PARAMETER AND CALLS MYCALLBACK FUNC WITH ERROR AND ARGUMENT AS ARG
+    const callBack = (error, response, body) => {
+      return myCallBack(error, body);
+    };
+    // THIS MAKES MAKE AN HTTP POST REQUEST USING THE REQUEST MODULE PASSING
+    request.post(options, callBack); // OPTIONS AND CALLBACK AS ARG
+  };
 
-}
-return {initializePayment, verifyPayment }
-}
-
-module.exports = payStack
+  // THIS FUNCTION TAKES TWO PARAMETER REF AND MYCALLBACK
+  const verifyPayment = (ref, myCallBack) => {
+    // THIS LINE MAKES AN HTTP REQ TO VERIFY A PAYMENT THR THE PAYSTACK API
+    const options = {
+      url:
+        "https://api.paystack.co/transaction/verify/" + encodeURIComponent(ref), //THE REFERENCE OF THE TRANSACTION WE WANT TO VERIFY
+      headers: {
+        authorization: process.env.PAYSTACK_SECRET_KEY,
+        "content-type": "application/json",
+        "cache-control": "no-cache",
+      },
+    };
+    const callBack = (error, response, body) => {
+      return myCallBack(error, body);
+    };
+    request(options, callBack);
+  };
+  //THIS RETURN AN OBJECT CONTAINING THE TWO FUNCTIONS TO BE ACCESESD OUTSIDE THE PAYSTACK FUNC
+  return { initializePayment, verifyPayment };
+};
+// THIS EXPORT THE FUNCTION MAKINH IT AVAILABLE IN OTHER MODULES
+module.exports = payStack;
