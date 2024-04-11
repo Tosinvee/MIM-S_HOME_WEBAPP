5e9316d64c1093e59e715c818729468093f2411b
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-04-11 21:09:15 +0100

    paymentmodel

---
+++ models/paymentModel.js
@@ -0,0 +1,33 @@
+const { required } = require('joi')
+const moongose = require('mongoose')
+
+const paymentSchema = new moongose.Schema({
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    amount:{
+        type:Number,
+        required:true
+    },
+    refernce:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    status:{
+        type:String,
+        required:true
+    }
+},
+{
+    timestamps: true
+}
+)
+const Payment = moongose.model(Payment,paymentSchema)
+
+module.exports = Payment
